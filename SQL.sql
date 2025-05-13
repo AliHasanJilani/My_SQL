07b1f6514ba025ca5e1f83b3923a7392a7e41d43
AliHasanJilani <s.alihasan757@gmail.com>
2025-05-13 18:36:07 +0530

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,15 @@
+select database();
+
+use sys;
+
+select database();
+
+show databases;
+
+show tables;
+
+select * from sys_config;
+
+select variable, value from sys_config;
+
+describe sys_config;
